3875cf1df110f5d8d04d007a2a404782300c8d72
Remi Ete <remi.ete@gmail.com>
2016-12-06 19:06:54 +0000

    Fixed number of required arguments

--- source/main/dqm4hep-send-request-response.cc
+++ source/main/dqm4hep-send-request-response.cc
@@ -34,7 +34,7 @@ using namespace dqm4hep::net;
 
 int main(int argc, char **argv)
 {
-  if(argc <= 3)
+  if(argc < 3)
   {
     std::cout << "Usage : dqm4hep-send-request-response type name [content]" << std::endl;
     return 1;
