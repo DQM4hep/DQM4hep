26492b70c358bf03714b843fcf086d9e13b83618
Remi Ete <remi.ete@gmail.com>
2017-03-22 12:26:22 +0000

    Removed LogLevel (from spdlog now) + removed UpdateMode

--- source/include/dqm4hep/DQM4HEP.h
+++ source/include/dqm4hep/DQM4HEP.h
@@ -5,22 +5,22 @@
  * Creation date : dim. oct. 5 2014
  *
  * This file is part of DQM4HEP libraries.
- * 
+ *
  * DQM4HEP is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * based upon these libraries are permitted. Any copy of these libraries
  * must include this copyright notice.
- * 
+ *
  * DQM4HEP is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with DQM4HEP.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * @author Remi Ete
  * @copyright CNRS , IPNL
  */
@@ -670,29 +670,6 @@ namespace dqm4hep {
       DQM_ALERT_TYPE_TABLE(GET_STR_COMPARE)
 	    else return OTHER_ALERT;
     }
-
-    //-------------------------------------------------------------------------------------------------
-
-    /** LogLevel enumerator
-     */
-    enum LogLevel
-    {
-      DEBUG = 0,
-      MESSAGE,
-      WARNING,
-      ERROR
-    };
-
-    //-------------------------------------------------------------------------------------------------
-
-    /** UpdateMode enumerator
-     */
-    enum UpdateMode
-    {
-      NO_UPDATE = 0,
-      REPLACE = 1,
-      APPEND = 2
-    };
 
     //-------------------------------------------------------------------------------------------------
 
