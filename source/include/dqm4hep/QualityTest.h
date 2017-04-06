24e4e804dee034092e8674e5f34b165d4011afa3
Remi Ete <remi.ete@gmail.com>
2017-04-06 01:48:05 +0000

    cosmetic changes

--- source/include/dqm4hep/QualityTest.h
+++ source/include/dqm4hep/QualityTest.h
@@ -5,22 +5,22 @@
  * Creation date : ven. f�vr. 20 2015
  *
  * This file is part of DQM4HEP libraries.
- * 
+ *
  * DQM4HEP is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * based upon these libraries are permitted. Any copy of these libraries
  * must include this copyright notice.
- * 
+ *
  * DQM4HEP is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with DQM4HEP.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * @author Remi Ete
  * @copyright CNRS , IPNL
  */
@@ -69,7 +69,7 @@ namespace dqm4hep {
       std::string           m_qualityTestName;
       std::string           m_qualityTestType;
       std::string           m_message;
-      Quality            m_quality;
+      Quality               m_quality;
       bool                  m_isSuccessful;
     };
 
