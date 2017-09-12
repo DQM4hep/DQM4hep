3e2741997c483244a5cd91d6ccb7289550d5b635
Remi Ete <remi.ete@gmail.com>
2017-09-12 17:06:57 +0000

    Added constness to getPlugin method

--- source/include/dqm4hep/PluginManager.h
+++ source/include/dqm4hep/PluginManager.h
@@ -4,22 +4,22 @@
  * Creation date : lun. ao�t 17 2015
  *
  * This file is part of DQM4HEP libraries.
- * 
+ *
  * DQM4HEP is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * based upon these libraries are permitted. Any copy of these libraries
  * must include this copyright notice.
- * 
+ *
  * DQM4HEP is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with DQM4HEP.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * @author Remi Ete
  * @copyright CNRS , IPNL
  */
@@ -78,7 +78,7 @@ namespace dqm4hep {
 
       /** Get the plug-in by name
        */
-      Plugin *getPlugin( const std::string &pluginName ) const;
+      const Plugin *getPlugin( const std::string &pluginName ) const;
 
       /** Get the plug-in clone. A new plug-in instance is allocated and returned
        *  to the user. Ownership of the plug-in transfered to the caller.
