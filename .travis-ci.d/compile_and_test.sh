6c958fdc2f2fe4c0f0fe02060851a0db724fba27
Remi Ete <remi.ete@gmail.com>
2017-10-27 16:02:25 +0000

    Forgot to switch on cmake tests

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -7,7 +7,7 @@ source dependencies/root/bin/thisroot.sh
 mkdir -p build
 cd build
 
-cmake -DINSTALL_DOC=OFF -Dxdrstream_DIR=$PWD/../dependencies/xdrstream -DCMAKE_MODULE_PATH=$PWD/../dependencies/dqm4hep/cmake ..
+cmake -DINSTALL_DOC=OFF -Dxdrstream_DIR=$PWD/../dependencies/xdrstream -DCMAKE_MODULE_PATH=$PWD/../dependencies/dqm4hep/cmake -DBUILD_TESTS=ON ..
 
 if [ $? -ne 0 ]; then
     echo "Failed to run cmake"
