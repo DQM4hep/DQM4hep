2e24a73b113687a9b9d2518eca1fb238d80fd8ae
Remi Ete <remi.ete@gmail.com>
2017-10-27 15:54:19 +0000

    Updated TravisCI

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
 ls -la
+
+source dependencies/root/bin/thisroot.sh
+
 mkdir -p build
 cd build
-cmake -DINSTALL_DOC=OFF -Dxdrstream_DIR=$XDRSTREAM_DIR -DCMAKE_MODULE_PATH=$DQM4HEP_DIR/cmake ..
+
+cmake -DINSTALL_DOC=OFF -Dxdrstream_DIR=$PWD/../dependencies/xdrstream -DCMAKE_MODULE_PATH=$PWD/../dependencies/cmake ..
+
+if [ $? -ne 0 ]; then
+    echo "Failed to run cmake"
+    exit 1
+fi
+
 make install -j 4
+
+if [ $? -ne 0 ]; then
+    echo "Failed to run make"
+    exit 1
+fi
+
 ctest -V
+
+if [ $? -ne 0 ]; then
+    echo "Failed to run cmake tests"
+    exit 1
+fi
