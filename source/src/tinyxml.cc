6d0c2070c4c86a93bf24a82ce77a5b30e24c1044
Ete Remi <remi.ete@desy.de>
2018-02-09 18:15:01 +0100

    Needed second formatting

--- source/src/tinyxml.cc
+++ source/src/tinyxml.cc
@@ -817,9 +817,9 @@ namespace dqm4hep {
       // Systems based on ASCII or a compatible character set use either LF  (Line feed, '\n', 0x0A, 10 in decimal) or
       // CR (Carriage return, '\r', 0x0D, 13 in decimal) individually, or CR followed by LF (CR+LF, 0x0D 0x0A)...
       //		* LF:    Multics, Unix and Unix-like systems (GNU/Linux, AIX, Xenix, Mac OS X, FreeBSD, etc.),
-      //BeOS, Amiga, RISC OS, and others
+      // BeOS, Amiga, RISC OS, and others
       //		* CR+LF: DEC RT-11 and most other early non-Unix, non-IBM OSes, CP/M, MP/M, DOS, OS/2, Microsoft
-      //Windows, Symbian OS
+      // Windows, Symbian OS
       //		* CR:    Commodore 8-bit machines, Apple II family, Mac OS up to version 9 and OS-9
 
       const char *p = buf; // the read head
--- source/src/tinyxmlparser.cc
+++ source/src/tinyxmlparser.cc
@@ -62,8 +62,9 @@ namespace dqm4hep {
     const unsigned char TIXML_UTF_LEAD_2 = 0xbfU;
 
     const int TiXmlBase::utf8ByteTable[256] = {
-        //	0	1	2	3	4	5	6	7	8	9	a	b	c	d	e
-        //f
+        //	0	1	2	3	4	5	6	7	8	9	a	b	c	d
+        //e
+        // f
         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // 0x00
         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // 0x10
         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // 0x20
