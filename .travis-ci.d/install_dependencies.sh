ecc961599dd277b76029313908f7e53b7ba42ca7
Ete Remi <remi.ete@desy.de>
2018-02-09 14:37:54 +0100

    Removed jsoncpp from build

--- .travis-ci.d/install_dependencies.sh
+++ .travis-ci.d/install_dependencies.sh
@@ -34,26 +34,5 @@ fi
 
 cd ../..
 
-# install jsoncpp
-git clone https://github.com/open-source-parsers/jsoncpp.git
-cd jsoncpp
-mkdir install
-export JSONCPP_INSTALL_DIR=$PWD/install
-git checkout 1.8.3
-mkdir -p build && cd build
-cmake -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=OFF -DJSONCPP_WITH_TESTS=OFF -DJSONCPP_WITH_WARNING_AS_ERROR=OFF -DCMAKE_INSTALL_PREFIX=${JSONCPP_INSTALL_DIR} ..
-
-if [ $? -ne 0 ]; then
-    echo "Failed to run jsoncpp cmake"
-    exit 1
-fi
-
-make install VERBOSE=1
-
-if [ $? -ne 0 ]; then
-    echo "Failed to run json make"
-    exit 1
-fi
-
 ls -la
 cd ..
