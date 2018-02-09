c394509ffb4e5fb1d233da1b551cdc5c4fd64032
Ete Remi <remi.ete@desy.de>
2018-02-09 18:16:03 +0100

    Needed re-formatting

--- source/src/tinyxmlparser.cc
+++ source/src/tinyxmlparser.cc
@@ -63,7 +63,7 @@ namespace dqm4hep {
 
     const int TiXmlBase::utf8ByteTable[256] = {
         //	0	1	2	3	4	5	6	7	8	9	a	b	c
-        //d
+        // d
         // e
         // f
         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // 0x00
