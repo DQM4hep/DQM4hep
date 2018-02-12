33d0acbc3341dea8250843e74bd89bff3b38d38e
Ete Remi <remi.ete@desy.de>
2018-02-12 14:53:02 +0100

    Implementation of ExactRefCompareTest finished. Waiting for merging PR 28 before

--- source/src/qtest/ExactRefCompareTest.cc
+++ source/src/qtest/ExactRefCompareTest.cc
@@ -36,6 +36,8 @@
 
 // -- root headers
 #include <TH1.h>
+#include <TGraph.h>
+#include <TGraph2D.h>
 #include <TMath.h>
 
 namespace dqm4hep {
@@ -70,7 +72,8 @@ namespace dqm4hep {
       
     private:
       void doHistogramTest(MonitorElementPtr monitorElement, QualityTestReport &report);
-      void doGraphTest(MonitorElementPtr monitorElement, QualityTestReport &report);
+      void doGraph1DTest(MonitorElementPtr monitorElement, QualityTestReport &report);
+      void doGraph2DTest(MonitorElementPtr monitorElement, QualityTestReport &report);
       void doIsEqualTest(MonitorElementPtr monitorElement, QualityTestReport &report);
       
     private:
@@ -91,7 +94,9 @@ namespace dqm4hep {
     //-------------------------------------------------------------------------------------------------
 
     ExactRefCompareTest::ExactRefCompareTest(const std::string &name)
-        : QualityTest("ExactRefCompare", name), m_compareUnderflow(true), m_compareOverflow(true) {
+        : QualityTest("ExactRefCompare", name), 
+        m_compareUnderflow(true), 
+        m_compareOverflow(true) {
       m_description = std::string("Compare the monitor element to its reference. ") + 
       "Look for an strict equality in terms of object content.\n" + 
       "For TH1 case (and all derived): \n" +
@@ -110,7 +115,6 @@ namespace dqm4hep {
     StatusCode ExactRefCompareTest::readSettings(const TiXmlHandle xmlHandle) {
 
       RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::readParameter(xmlHandle, "CompareOverflow", m_compareOverflow));
-      
       RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::readParameter(xmlHandle, "CompareUnderflow", m_compareUnderflow));
 
       return STATUS_CODE_SUCCESS;
@@ -138,11 +142,12 @@ namespace dqm4hep {
       const std::string objectClass(monitorElement->object()->ClassName());
       const std::string referenceClass(monitorElement->reference()->ClassName());
       const bool isHistogram = monitorElement->object()->InheritsFrom("TH1");
-      const bool isGraph = monitorElement->object()->InheritsFrom("TGraph");
+      const bool isGraph1D = monitorElement->object()->InheritsFrom("TGraph");
+      const bool isGraph2D = monitorElement->object()->InheritsFrom("TGraph2D");
       const bool sameType = (objectClass == referenceClass);
       
       if(!sameType) {
-        report.m_message = "No reference attached to monitor element";
+        report.m_message = "Objet and reference do not have the same type";
         report.m_quality = 0.f;
         throw StatusCodeException(STATUS_CODE_FAILURE);
       }
@@ -150,8 +155,11 @@ namespace dqm4hep {
       if (isHistogram) {
         this->doHistogramTest(monitorElement, report);
       }
-      else if(isGraph) {
-        this->doGraphTest(monitorElement, report);
+      else if(isGraph1D) {
+        this->doGraph1DTest(monitorElement, report);
+      }
+      else if(isGraph2D) {
+        this->doGraph2DTest(monitorElement, report);
       }
       else {
         this->doIsEqualTest(monitorElement, report);
@@ -205,6 +213,8 @@ namespace dqm4hep {
       if(nDifferentBins != 0) {
         report.m_message = "Histogram and reference are different (difference = " + typeToString(nDifferentBins) + ")";
         // TODO uncomment after PR #28 merged
+        // Quality estimate as the percentage of different points. 
+        // This factor is scaled to the error limit to make sure the quality flag is set to ERROR
         report.m_quality = /*this->errorLimit() **/ (static_cast<float>(nBins) - static_cast<float>(nDifferentBins)) / static_cast<float>(nBins);
       }
       else {
@@ -213,19 +223,120 @@ namespace dqm4hep {
         report.m_quality = 1.f;
       }
       
-      report.m_extraInfos["nDifferentBins"] = nDifferentBins;
+      report.m_extraInfos["nDifferences"] = nDifferentBins;
     }
     
     //-------------------------------------------------------------------------------------------------
     
-    void ExactRefCompareTest::doGraphTest(MonitorElementPtr monitorElement, QualityTestReport &report) {
+    void ExactRefCompareTest::doGraph1DTest(MonitorElementPtr monitorElement, QualityTestReport &report) {
       
+      TGraph *graph = monitorElement->objectTo<TGraph>();
+      TGraph *reference = monitorElement->referenceTo<TGraph>();
+      const unsigned int nPoints(graph->GetN());
+      const unsigned int nPointsRef(reference->GetN());
+      
+      if(nPoints != nPointsRef) {
+        report.m_message = "Graph and reference do not have the same number of points";
+        report.m_quality = 0.f;
+        throw StatusCodeException(STATUS_CODE_FAILURE);
+      }
+      
+      unsigned int nDifferentPoints(0);
+      
+      for(Int_t point = 0 ; point < nPoints ; ++point) {
+        Double_t x, y, xRef, yRef;
+        graph->GetPoint(point, x, y);
+        reference->GetPoint(point, xRef, yRef);
+        
+        const bool xAreDifferent(!(fabs(x-xRef) < std::numeric_limits<Double_t>::epsilon()));
+        const bool yAreDifferent(!(fabs(y-yRef) < std::numeric_limits<Double_t>::epsilon()));
+        
+        if(xAreDifferent || yAreDifferent) {
+          ++nDifferentPoints;
+        }
+      }
+      
+      if(nDifferentPoints != 0) {
+        report.m_message = "Graph and reference are different (difference = " + typeToString(nDifferentPoints) + ")";
+        // TODO uncomment after PR #28 merged
+        report.m_quality = /*this->errorLimit() **/ (static_cast<float>(nPoints) - static_cast<float>(nDifferentPoints)) / static_cast<float>(nPoints);
+      }
+      else {
+        report.m_message = "Graph and reference are equal !";
+        // TODO uncomment after PR #28 merged
+        report.m_quality = 1.f;
+      }
+      
+      report.m_extraInfos["nDifferences"] = nDifferentPoints;
+    }
+    
+    //-------------------------------------------------------------------------------------------------
+    
+    void ExactRefCompareTest::doGraph2DTest(MonitorElementPtr monitorElement, QualityTestReport &report) {
+      
+      TGraph2D *graph = monitorElement->objectTo<TGraph2D>();
+      TGraph2D *reference = monitorElement->referenceTo<TGraph2D>();
+      const unsigned int nPoints(graph->GetN());
+      const unsigned int nPointsRef(reference->GetN());
+      
+      if(nPoints != nPointsRef) {
+        report.m_message = "Graph and reference do not have the same number of points";
+        report.m_quality = 0.f;
+        throw StatusCodeException(STATUS_CODE_FAILURE);
+      }
+      
+      unsigned int nDifferentPoints(0);
+      
+      for(Int_t point = 0 ; point < nPoints ; ++point) {
+        const Double_t x = graph->GetX()[point];
+        const Double_t y = graph->GetY()[point];
+        const Double_t z = graph->GetZ()[point];
+        const Double_t xRef = reference->GetX()[point];
+        const Double_t yRef = reference->GetY()[point];
+        const Double_t zRef = reference->GetZ()[point];
+        
+        const bool xAreDifferent(!(fabs(x-xRef) < std::numeric_limits<Double_t>::epsilon()));
+        const bool yAreDifferent(!(fabs(y-yRef) < std::numeric_limits<Double_t>::epsilon()));
+        const bool zAreDifferent(!(fabs(z-zRef) < std::numeric_limits<Double_t>::epsilon()));
+        
+        if(xAreDifferent || yAreDifferent || zAreDifferent) {
+          ++nDifferentPoints;
+        }
+      }
+      
+      if(nDifferentPoints != 0) {
+        report.m_message = "Graph and reference are different (difference = " + typeToString(nDifferentPoints) + ")";
+        // TODO uncomment after PR #28 merged
+        report.m_quality = /*this->errorLimit() **/ (static_cast<float>(nPoints) - static_cast<float>(nDifferentPoints)) / static_cast<float>(nPoints);
+      }
+      else {
+        report.m_message = "Graph and reference are equal !";
+        // TODO uncomment after PR #28 merged
+        report.m_quality = 1.f;
+      }
+      
+      report.m_extraInfos["nDifferences"] = nDifferentPoints;
     }
     
     //-------------------------------------------------------------------------------------------------
     
     void ExactRefCompareTest::doIsEqualTest(MonitorElementPtr monitorElement, QualityTestReport &report) {
       
+      TObject *object = monitorElement->object();
+      TObject *reference = monitorElement->reference();
+      
+      const bool equal(object->IsEqual(reference));
+      
+      if(!equal) {
+        report.m_message = "Object and reference are different (using TObject::IsEqual(ref)).";
+        report.m_quality = 0.f; // FIXME: a bit hard no ? ...
+      }
+      else {
+        report.m_message = "Object and reference are equal (using TObject::IsEqual(ref))";
+        report.m_quality = 1.f;
+      }
+      
+      report.m_extraInfos["nDifferences"] = 1;
     }
 
     DQM_PLUGIN_DECL(ExactRefCompareTestFactory, "ExactRefCompareTest");
