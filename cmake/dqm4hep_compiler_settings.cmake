85d9d97d073f507cb29a4c6094073de0c7c66866
Remi Ete <remi.ete@gmail.com>
2018-04-16 01:36:11 +0200

    fixed wrong cxx flags append

--- cmake/dqm4hep_compiler_settings.cmake
+++ cmake/dqm4hep_compiler_settings.cmake
@@ -36,7 +36,7 @@ macro( DQM4HEP_SET_CXX_FLAGS )
   check_cxx_compiler_flag( "-std=c++11" COMPILER_SUPPORTS_CXX11 )
   
   if( COMPILER_SUPPORTS_CXX11 )
-    set( CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} -std=c++11 )
+    set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11" )
   else()
     message( SEND_ERROR "${PROJECT_NAME} requires C++11 support. Please upgrade your compiler !" )
   endif()
