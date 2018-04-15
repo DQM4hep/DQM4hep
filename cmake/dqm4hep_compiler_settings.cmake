fa19e61339e70d6bd4a91873251a3a1bcd7a8ac7
Remi Ete <remi.ete@gmail.com>
2018-04-16 00:40:55 +0200

    Added new macro dqm4hep_set_cxx_flags

--- cmake/dqm4hep_compiler_settings.cmake
+++ cmake/dqm4hep_compiler_settings.cmake
@@ -1,11 +1,43 @@
 
-# Require C++11
-include( CheckCXXCompilerFlag )
+option( DQM4HEP_WARNING_AS_ERROR    "Whether to add -Werror flag to cxx flags" OFF )
+option( DQM4HEP_EXTRA_WARNINGS      "Whether to add -Wextra flag to cxx flags" OFF )
 
-check_cxx_compiler_flag( "-std=c++11" COMPILER_SUPPORTS_CXX11 )
+macro( DQM4HEP_SET_CXX_FLAGS )
+  # Require C++11
+  include( CheckCXXCompilerFlag )
+  
+  set( COMPILER_FLAGS -Wunused-value -Wall -Wextra -pedantic -Wshadow -Wformat-security -Wno-long-long -Wdeprecated -Wreturn-type )
+  
+  if( DQM4HEP_WARNING_AS_ERROR )
+    set( COMPILER_FLAGS ${COMPILER_FLAGS} -Werror )
+  endif()
+  
+  if( DQM4HEP_EXTRA_WARNINGS )
+    set( COMPILER_FLAGS ${COMPILER_FLAGS} -Wextra )
+  endif()
+  
+  foreach( FLAG ${COMPILER_FLAGS} )
+    ## need to replace the minus or plus signs from the variables, because it is passed
+    ## as a macro to g++ which causes a warning about no whitespace after macro
+    ## definition
+    string( REPLACE "-" "_" FLAG_WORD ${FLAG} )
+    string( REPLACE "+" "P" FLAG_WORD ${FLAG_WORD} )
 
-if( COMPILER_SUPPORTS_CXX11 )
-  set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11" )
-else()
-	message( SEND_ERROR "${PROJECT_NAME} requires C++11 support. Please upgrade your compiler !" )
-endif()
+    check_cxx_compiler_flag( "${FLAG}" CXX_FLAG_WORKS_${FLAG_WORD} )
+    
+    if( ${CXX_FLAG_WORKS_${FLAG_WORD}} )
+      message( STATUS "Adding ${FLAG} to CXX_FLAGS" )
+      set( CMAKE_CXX_FLAGS "${FLAG} ${CMAKE_CXX_FLAGS} ")
+    else()
+      message( STATUS "NOT Adding ${FLAG} to CXX_FLAGS" )
+    endif()
+  endforeach()
+  
+  check_cxx_compiler_flag( "-std=c++11" COMPILER_SUPPORTS_CXX11 )
+  
+  if( COMPILER_SUPPORTS_CXX11 )
+    set( CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} -std=c++11 )
+  else()
+    message( SEND_ERROR "${PROJECT_NAME} requires C++11 support. Please upgrade your compiler !" )
+  endif()
+endmacro()
