a2c5371384121fd7f4de7fb5f9e0b37e41298187
Remi Ete <remi.ete@gmail.com>
2018-04-22 01:26:43 +0200

    Temporary switch off -Werror

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -7,7 +7,7 @@ source dependencies/root/bin/thisroot.sh
 mkdir -p build
 cd build
 
-cmake -DDQM4HEP_DOXYGEN_DOC=OFF -Dxdrstream_DIR=$PWD/../dependencies/xdrstream -DCMAKE_MODULE_PATH=$PWD/../dependencies/dqm4hep/cmake -DDQM4HEP_TESTING=ON -DDQM4HEP_WARNING_AS_ERROR=ON -DDQM4HEP_DEV_WARNINGS=ON ..
+cmake -DDQM4HEP_DOXYGEN_DOC=OFF -Dxdrstream_DIR=$PWD/../dependencies/xdrstream -DCMAKE_MODULE_PATH=$PWD/../dependencies/dqm4hep/cmake -DDQM4HEP_TESTING=ON -DDQM4HEP_WARNING_AS_ERROR=OFF -DDQM4HEP_DEV_WARNINGS=ON ..
 
 if [ $? -ne 0 ]; then
     echo "Failed to run cmake"
