652d5557ae5fdbccd755f30c499037832522285a
Remi Ete <remi.ete@gmail.com>
2018-04-24 00:02:07 +0200

    Fixed double cmake command call

--- .travis-ci.d/install_dependencies.sh
+++ .travis-ci.d/install_dependencies.sh
@@ -38,8 +38,6 @@ cd ../..
 git clone https://github.com/dqm4hep/dqm4hep-core.git
 cd dqm4hep-core
 mkdir -p build && cd build
-cmake -DDQM4HEP_DOXYGEN_DOC=OFF -Dxdrstream_DIR=$PWD/../dependencies/xdrstream -DCMAKE_MODULE_PATH=$PWD/../dependencies/dqm4hep/cmake -DDQM4HEP_TESTING=ON -DDQM4HEP_WARNING_AS_ERROR=ON -DDQM4HEP_DEV_WARNINGS=ON ..
-
 cmake -DDQM4HEP_DOXYGEN_DOC=OFF -Dxdrstream_DIR=$PWD/../../xdrstream -DCMAKE_MODULE_PATH=$PWD/../../dqm4hep/cmake -DDQM4HEP_TESTING=OFF -DDQM4HEP_WARNING_AS_ERROR=OFF -DDQM4HEP_DEV_WARNINGS=OFF ..
 
 if [ $? -ne 0 ]; then
