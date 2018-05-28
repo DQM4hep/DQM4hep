7f82f8a6f92b586d297070170ec1c5538ebbaafe
Antoine Pingault <antoine.pingault@gmail.com>
2018-05-28 16:14:31 +0200

    Adapt memstats exec to changes from last commit

--- source/main/dqm4hep-memstats.cc
+++ source/main/dqm4hep-memstats.cc
@@ -36,36 +36,26 @@
 #define fmt_value(val) std::setw(16) << std::left << std::setprecision(1) << std::fixed << val
 
 int main(int /*argc*/, char ** /*argv[]*/) {
-  
-  while(1) {
-    
-    dqm4hep::core::MemoryStats stats;
-    dqm4hep::core::memStats(stats);
-    
-    std::cout << std::setw(10) << std::left << "Memory" 
-              << fmt_value("Total (Mb)") 
-              << fmt_value("Used (Mb)")
-              << fmt_value("Process (Mb)") 
-              << fmt_value("Used (%)")
-              << fmt_value("Proc used (%)")
-              << std::endl;
-    
-    std::cout << std::setw(10) << std::left << "Resident" 
-              << fmt_value(stats.rsstot) 
-              << fmt_value(stats.rssused)
-              << fmt_value(stats.rssproc)
-              << fmt_value((stats.rssused/(stats.rsstot*1.))*100)
-              << fmt_value((stats.rssproc/(stats.rsstot*1.))*100)
-              << std::endl;
-    
-    std::cout << std::setw(10) << std::left << "Virtual" 
-              << fmt_value(stats.vmtot)
-              << fmt_value(stats.vmused) 
-              << fmt_value(stats.vmproc) 
-              << fmt_value((stats.vmused/(stats.vmtot*1.))*100)
-              << fmt_value((stats.vmproc/(stats.vmtot*1.))*100.)
-              << std::endl;
-    
+
+  while (1) {
+
+    dqm4hep::core::MemoryStats memInfo;
+    dqm4hep::core::memStats(memInfo);
+
+    dqm4hep::core::ProcessStats procInfo;
+    dqm4hep::core::procStats(procInfo);
+
+    std::cout << std::setw(10) << std::left << "Memory" << fmt_value("Total (Mb)") << fmt_value("Used (Mb)")
+              << fmt_value("Process (Mb)") << fmt_value("Used (%)") << fmt_value("Proc used (%)") << std::endl;
+
+    std::cout << std::setw(10) << std::left << "Resident" << fmt_value(memInfo.rssTot) << fmt_value(memInfo.rssUsed)
+              << fmt_value(procInfo.rss / 1024.) << fmt_value((memInfo.rssUsed / (memInfo.rssTot * 1.)) * 100)
+              << fmt_value(((procInfo.rss / 1024.) / (memInfo.rssTot * 1.)) * 100) << std::endl;
+
+    std::cout << std::setw(10) << std::left << "Virtual" << fmt_value(memInfo.vmTot) << fmt_value(memInfo.vmUsed)
+              << fmt_value(procInfo.vm / 1024.) << fmt_value((memInfo.vmUsed / (memInfo.vmTot * 1.)) * 100)
+              << fmt_value(((procInfo.vm / 1024.) / (memInfo.vmTot * 1.)) * 100.) << std::endl;
+
     std::cout << std::endl;
     dqm4hep::core::sleep(std::chrono::seconds(2));
   }
