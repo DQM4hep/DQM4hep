77aa30765c83b859481298ae40febb3ff51a91c1
Antoine Pingault <antoine.pingault@gmail.com>
2018-05-29 19:49:43 +0200

    Update cpuStats exec with process cpu load report
    We now need processStats to exist outside of the while loop so we can keep track of the cpu consumption

--- source/main/dqm4hep-cpustats.cc
+++ source/main/dqm4hep-cpustats.cc
@@ -32,17 +32,20 @@
 // -- dqm4hep headers
 #include <dqm4hep/Internal.h>
 #include <iomanip>
+#include <sys/time.h>
 
 #define wfmt_value(width, val) std::setw(width) << std::left << std::setprecision(1) << std::fixed << val
 #define fmt_value(val) wfmt_value(10, val)
 
 int main(int /*argc*/, char ** /*argv[]*/) {
 
+  struct timeval timeStart;
+  gettimeofday(&timeStart, NULL);
+  dqm4hep::core::ProcessStats procInfo;
+  procInfo.lastPollTime = timeStart;
   while (1) {
     dqm4hep::core::CpuStats cpuInfo;
     dqm4hep::core::cpuStats(cpuInfo);
-
-    dqm4hep::core::ProcessStats procInfo;
     dqm4hep::core::procStats(procInfo);
 
     // System Info
@@ -55,11 +58,10 @@ int main(int /*argc*/, char ** /*argv[]*/) {
               << fmt_value(cpuInfo.tot) << fmt_value(cpuInfo.idle) << std::endl;
 
     // Process Info
-    std::cout << std::setw(16) << std::left << "Process Load" << fmt_value("User (%)") << fmt_value("Sys (%)")
+    std::cout << std::setw(16) << std::left << "Process Load" << fmt_value("User (%)") << fmt_value("Sys (%)") << fmt_value("Tot (%)")
               << wfmt_value(20, "User cpuTime (s)") << wfmt_value(20, "Sys cpuTime (s)") << std::endl;
 
-    std::cout << std::setw(16) << std::left << " " << fmt_value(procInfo.cpuUser) << fmt_value(procInfo.cpuSys)
-              << wfmt_value(20, procInfo.cpuTimeUser) << wfmt_value(20, procInfo.cpuTimeSys) << std::endl;
+    std::cout << std::setw(16) << std::left << " " << fmt_value(procInfo.cpuUser) << fmt_value(procInfo.cpuSys) << fmt_value(procInfo.cpuTot) << wfmt_value(20, procInfo.cpuTimeUser) << wfmt_value(20, procInfo.cpuTimeSys) << std::endl;
     std::cout << std::endl;
   }
 
