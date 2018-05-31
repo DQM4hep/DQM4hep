c03b29436ed02e4a2f44c1e162265148d821444d
Remi Ete <remi.ete@gmail.com>
2018-05-31 12:25:12 +0200

    Use TravisCI env var instead of git command to get commit id

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -3,14 +3,13 @@
 ls -la
 
 source dependencies/root/bin/thisroot.sh
-export CURRENT_COMMIT_ID=`git rev-parse HEAD`
 
 git clone https://github.com/dqm4hep/dqm4hep.git
 cd dqm4hep
 mkdir build
 cd build
 
-cmake -DDQM4HEP_USE_MASTER=ON -DDQM4HEP_DOXYGEN_DOC=OFF -DDQM4HEP_TESTING=ON -DDQM4HEP_WARNING_AS_ERROR=ON -DDQM4HEP_DEV_WARNINGS=ON -DDQMCore_repository=$PWD/../.. -DDQMCore_version=$CURRENT_COMMIT_ID ..
+cmake -DDQM4HEP_USE_MASTER=ON -DDQM4HEP_DOXYGEN_DOC=OFF -DDQM4HEP_TESTING=ON -DDQM4HEP_WARNING_AS_ERROR=ON -DDQM4HEP_DEV_WARNINGS=ON -DDQMCore_repository=$PWD/../.. -DDQMCore_version=$TRAVIS_COMMIT ..
 
 if [ $? -ne 0 ]; then
     echo "Failed to run cmake"
