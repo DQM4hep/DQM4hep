c86887ee7e746cf59a49a8d806ddb188a266f874
Antoine Pingault <antoine.pingault@gmail.com>
2018-06-02 13:25:14 +0200

    Disable running ctests from make on osx
    For some reason running ctests from make leads to a `Child aborted` exception being thrown.
    Running the tests manually works without any problem though...

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -9,7 +9,12 @@ cd dqm4hep
 mkdir build
 cd build
 
-cmake -DDQM4HEP_USE_MASTER=ON -DDQM4HEP_DOXYGEN_DOC=OFF -DDQM4HEP_TESTING=ON -DDQM4HEP_WARNING_AS_ERROR=ON -DDQM4HEP_DEV_WARNINGS=ON -DDQMCore_repository=$TRAVIS_BUILD_DIR -DDQMCore_version=$TRAVIS_COMMIT ..
+# Disable autamatic testing within make on osx. See comment below
+if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then export DQM4HEP_TESTING="OFF";
+else export DQM4HEP_TESTING="ON";
+fi
+
+cmake -DDQM4HEP_USE_MASTER=ON -DDQM4HEP_DOXYGEN_DOC=OFF -DDQM4HEP_TESTING=$DQM4HEP_TESTING -DDQM4HEP_WARNING_AS_ERROR=ON -DDQM4HEP_DEV_WARNINGS=ON -DDQMCore_repository=$TRAVIS_BUILD_DIR -DDQMCore_version=$TRAVIS_COMMIT ..
 
 if [ $? -ne 0 ]; then
     echo "Failed to run cmake"
@@ -30,3 +35,14 @@ fi
 $LDD_TOOL ./lib/*.$LIB_EXT
 $LDD_TOOL ./bin/*
 
+# For some reason, running ctest from within make leads to an instantaneous Child aborted exception being thrown on osx. Running the tests manually works without trouble though...
+if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
+    cd DQMCore-prefix/src/DQMCore-build/
+    ctest -V
+
+    if [ $? -ne 0 ]; then
+        echo "Failed to run cmake tests"
+        exit 1
+    fi
+    cd -
+fi
