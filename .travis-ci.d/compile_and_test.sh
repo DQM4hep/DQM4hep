fe61a27115a2beefab5f1f9c215d8a3cd54706aa
Ete Remi <remi.ete@desy.de>
2018-08-16 15:46:16 +0200

    Removed commented out code

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -7,11 +7,6 @@ source dependencies/root/bin/thisroot.sh
 mkdir -p build
 cd build
 
-# Disable automatic testing within make on osx. See comment below
-# if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then export DQM4hep_TESTING="OFF";
-# else export DQM4HEP_TESTING="ON";
-# fi
-
 cmake -DDQM4hep_DOXYGEN_DOC=OFF\
  -DDQM4hep_TESTING=ON \
  -DDQM4hep_WARNING_AS_ERROR=ON \
@@ -44,21 +39,4 @@ if [ $? -ne 0 ]; then
     echo "Failed to run cmake tests"
     exit 1
 fi
-# 
-# # For some reason, running ctest from within make leads to an instantaneous Child aborted exception being thrown on osx. Running the tests manually works without trouble though...
-# pkgList="xdrstream DQMCore DQMNet DQMOnline"
-# 
-# if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
-#     for pkg in $pkgList
-#         do cd $pkg-prefix/src/$pkg-build/
-#         ctest -V
-# 
-#         if [ $? -ne 0 ]; then
-#             echo "Failed to run cmake tests"
-#             exit 1
-#         fi
-#         cd -
-#     done
-# fi
-
 
