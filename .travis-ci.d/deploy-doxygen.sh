8358c4902178cbdc9ffd34635e7eb8c0abdf4c4b
Remi Ete <remi.ete@gmail.com>
2018-08-19 03:00:02 +0200

    Re-adapted doxygen build in TravisCI

--- .travis-ci.d/deploy-doxygen.sh
+++ .travis-ci.d/deploy-doxygen.sh
@@ -5,14 +5,14 @@ pkgVersion=""
 repository=$(basename $TRAVIS_REPO_SLUG | tr '[:upper:]' '[:lower:]')
 username=$(dirname $TRAVIS_REPO_SLUG | tr '[:upper:]' '[:lower:]')
 
-if [ ! "${CXX}" = "g++" ] || [ ! "${TRAVIS_OS_NAME}" = "linux" ] || [ "${TRAVIS_EVENT_TYPE}" = "cron" ]
+if [ ! "${DQM4hep_DOXYGEN_DOC}" = "ON" ]
 then
   echo "Build and push doxygen only one per push"
   exit 0
 fi
 
 # check correct user name
-if [ ! "${username}" = "dqm4hep" ]
+if [ ! "${username}" = "DQM4hep" ]
 then
   echo "Invalid username for pushing doxygen !"
   echo "Username: ${username}, repository: ${repository}"
@@ -39,30 +39,10 @@ else
   exit 0
 fi
 
-cd "$TRAVIS_BUILD_DIR"
-
-source dependencies/root/bin/thisroot.sh
-
-cd dqm4hep/build
-
-# activate doxygen building target
-cmake -DDQM4HEP_DOXYGEN_DOC=ON ..
-
-if [ $? -ne 0 ]; then
-    echo "Failed to run cmake"
-    exit 1
-fi
-
-# build doxygen documentation
-make install
-
-if [ $? -ne 0 ]; then
-    echo "Failed to run make doc"
-    exit 1
-fi
+cd $TRAVIS_BUILD_DIR
 
 # get the doxygen package
-git clone https://rete:$GITHUB_ACCESS_TOKEN@github.com/dqm4hep/dqm4hep-doxygen.git --branch=gh-pages
+git clone https://rete:$GITHUB_ACCESS_TOKEN@github.com/DQM4hep/dqm4hep-doxygen.git --branch=gh-pages
 cd dqm4hep-doxygen
 
 # add (if not exists) the package version to the web page
@@ -77,11 +57,11 @@ cd doxygen/${doxygenDirectory}
 rm -rf *
 
 # copy the new one in place
-cp -r $TRAVIS_BUILD_DIR/dqm4hep/doc/DQMCore/html/* .
+cp -r $TRAVIS_BUILD_DIR/doc/Doxygen/html/* .
 
 # commit the new doc
 git add ./*
 git -c user.name='travis' -c user.email='travis' commit -m "Updated ${doxygenDirectory} doxygen documentation, build commit: $TRAVIS_COMMIT"
 git remote rm origin
-git remote add origin https://rete:$GITHUB_ACCESS_TOKEN@github.com/dqm4hep/dqm4hep-doxygen.git
+git remote add origin https://rete:$GITHUB_ACCESS_TOKEN@github.com/DQM4hep/dqm4hep-doxygen.git
 git push -fq origin gh-pages >/dev/null 2>&1
