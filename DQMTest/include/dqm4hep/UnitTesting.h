7a7b30dc228dca7880691723f26d3a7b3fff7e79
Ete Remi <remi.ete@desy.de>
2018-08-21 16:28:55 +0200

    Changed all source header to point on correct licensing terms, copyrights and authors

--- DQMTest/include/dqm4hep/UnitTesting.h
+++ DQMTest/include/dqm4hep/UnitTesting.h
@@ -1,10 +1,19 @@
+//==========================================================================
+//  DQM4hep a data quality monitoring software for high energy physics 
+//--------------------------------------------------------------------------
+//
+// For the licensing terms see $DQM4hep_DIR/LICENSE.
+// For the list of contributors see $DQM4hep_DIR/AUTHORS.
+//
+// Author     : R.Ete
+//====================================================================
+
 #ifndef DQM4HEP_UNITTESTING_H
 #define DQM4HEP_UNITTESTING_H
 
 // -- dqm4hep headers
 #include <dqm4hep/Internal.h>
 #include <dqm4hep/Logging.h>
-
 
 namespace dqm4hep {
 
