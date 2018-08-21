3bb3bee1e176c2868156db20af73c9845fd17473
Remi Ete <remi.ete@gmail.com>
2018-08-21 23:21:47 +0200

    Removed authentification from wget command to get coverity scan tarball

--- .travis-ci.d/coverity_scan.sh
+++ .travis-ci.d/coverity_scan.sh
@@ -10,7 +10,7 @@ export description=`date`
 export COVERITY_REPO=`echo ${TRAVIS_REPO_SLUG} | sed 's/\//\%2F/g'`
 
 # install coverity scan
-wget https://scan.coverity.com/download/Linux --post-data "token=${COVERITY_SCAN_TOKEN}&project=${COVERITY_REPO}" -O coverity_tool.tgz
+wget https://scan.coverity.com/download/Linux -O coverity_tool.tgz
 mkdir cov-analysis-Linux
 tar -xf coverity_tool.tgz -C cov-analysis-Linux --strip-components=2 &> /dev/null
 export PATH=$PWD/cov-analysis-Linux/bin:$PATH
